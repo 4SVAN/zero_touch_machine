8e8e15b21bc6b9819191d50bcaf06264f427a9a1
Haoxuan ZHANG <macus4svan@gmail.com>
2023-06-09 14:34:35 +0200

    initial commit

--- loop.py
+++ loop.py
@@ -2,7 +2,7 @@ import random as rd
 import numpy as np
 
 def gau_generator():
-    gr = np.random.normal(0.5,0.5,10)
+    gr = np.random.normal(0.0,1.0,10)
     return gr
     
 print(gau_generator())
